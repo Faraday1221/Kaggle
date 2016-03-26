2f582dad70ff975d39a4d1ab87a99393c0e71351
Faraday1221 <faraday1221@gmail.com>
2016-03-25 21:53:12 -0400

    added first file

---
+++ Titanic/first_steps.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import numpy as np
+import pandas as pd
+import os
+
+filename = 'test.csv'
+path = os.path.dirname(__file__)+'/data/'
+
+# read in the dataset
+x = pd.read_csv(s.path.join(path,filename))
